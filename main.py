1d0a88c91cf2cc8c22ce4182af483f598d32140f
LoganLeeTwentyThree <loftylogan@gmail.com>
2025-03-29 11:33:14 -0500

    Attempt 2

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import socket
 
 nodeDict = {}
 
-destination = input("Enter a website:")
+destination = input("Enter a website: ")
 hops = traceroute(socket.gethostbyname(destination))
 
 last_distance = 0
+previousAddress = ""
 for hop in hops:
     print("o3o")
     name, alias, addresslist = socket.gethostbyaddr(hop.address)
-    nodeDict[hop.address] = name
-    #if last_distance + 1 != hop.distance:
-    #    print('Some gateways are not responding')
+    
+    if last_distance + 1 != hop.distance:
+        print('Some gateways are not responding')
+    else:
+        nodeDict[hop.address] = []
+        if previousAddress:
+            nodeDict[previousAddress].append(hop.address)
+        previousAddress = hop.address
+
     last_distance = hop.distance
 
 print(nodeDict)
