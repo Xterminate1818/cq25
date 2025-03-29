825c9c390e47a9fde981fb06a62b5dcfae968ee0
LoganLeeTwentyThree <loftylogan@gmail.com>
2025-03-29 11:14:45 -0500

    HELLO :D

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from icmplib import traceroute
+import socket
 
+nodeDict = {}
+
+destination = input("Enter a website:")
+hops = traceroute(socket.gethostbyname(destination))
+
+last_distance = 0
+for hop in hops:
+    print("o3o")
+    name, alias, addresslist = socket.gethostbyaddr(hop.address)
+    nodeDict[hop.address] = name
+    #if last_distance + 1 != hop.distance:
+    #    print('Some gateways are not responding')
+    last_distance = hop.distance
+
+print(nodeDict)
