5cf468c2a2c4673bf6ca78279daad291c537c75f
LoganLeeTwentyThree <loftylogan@gmail.com>
2025-03-29 11:43:37 -0500

    Added repeat functionality

--- main.py
+++ main.py
@@ -2,24 +2,41 @@ from icmplib import traceroute
 import socket
 
 nodeDict = {}
+running = True
+while(running):
 
-destination = input("Enter a website: ")
-hops = traceroute(socket.gethostbyname(destination))
+    destination = input("Enter a website: ")
+    hops = traceroute(socket.gethostbyname(destination))
 
-last_distance = 0
-previousAddress = ""
-for hop in hops:
-    print("o3o")
-    name, alias, addresslist = socket.gethostbyaddr(hop.address)
-    
-    if last_distance + 1 != hop.distance:
-        print('Some gateways are not responding')
-    else:
-        nodeDict[hop.address] = []
-        if previousAddress:
-            nodeDict[previousAddress].append(hop.address)
-        previousAddress = hop.address
+    last_distance = 0
+    previousAddress = ""
+    for hop in hops:
+        #name, alias, addresslist = socket.gethostbyaddr(hop.address)
+        
+        if last_distance + 1 != hop.distance:
+            print('Some gateways are not responding')
+        else:
+            if not nodeDict[hop.address]:
+                nodeDict[hop.address]= []
+            
+            if previousAddress:
+                nodeDict[previousAddress].append(hop.address)
+            previousAddress = hop.address
 
-    last_distance = hop.distance
+        last_distance = hop.distance
+
+    correctInput = False
+    while(not correctInput):
+        keepGoing = ""
+        keepGoing = input("Input Another?(y/N)").lower()
+        if(keepGoing == "n"):
+            correctInput = True
+            running = False
+        elif(keepGoing == "y"):
+            correctInput = True
+        else:
+            continue
+
+
 
 print(nodeDict)
